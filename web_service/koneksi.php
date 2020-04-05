7fe287ece5b59ece8592da622c3dd76b43719e0e
dilanreal <kuliahkuunisbank@gmail.com>
2020-04-05 19:07:57 +0700

    first-commit

---
+++ web_service/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+ define('HOST','localhost');
+ define('USER','id12806331_latihan_cordova');
+ define('PASS','admin123');
+ define('DB','id12806331_cordova');
+ $con = mysqli_connect("localhost","id12806331_latihan_cordova","admin123","id12806331_cordova") or die ("could not connect database");
+ ?>
